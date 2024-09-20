e1aa5237997935ffce9916ba1891ab1728480e37
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-09-20 23:01:20 +0700

    add main to run batch to automate all git submodule update and run compose

---
+++ main.bash
@@ -0,0 +1,9 @@
+cd midterm-test
+git fetch
+git pull https://github.com/Kiratopat-s/midterm-test main
+cd ..
+cd workflow-fe
+git fetch
+git pull https://github.com/Kiratopat-s/workflow-fe Kirato
+cd ..
+docker-compose up --build
